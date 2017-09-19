212bc31f96de625b3854a74a9697c4769f87b802
Eryk <rcksorto@yahoo.com>
2017-09-19 08:46:55 -0700

    Colour 2D balls

--- src/homework-one.cc
+++ src/homework-one.cc
@@ -1,17 +1,56 @@
 #include "homework-one.h"
 
-void Display() {
-  glClear(GL_COLOR_BUFFER_BIT);
+#include <unistd.h>
+
+void Ball::Draw() {
   glBegin(GL_POLYGON);
 
-  // Vertex points here
+  glColor3f(red, green, blue);
+
+  for (float theta = 0.0f; theta < 1.0f; theta += 0.01f) {
+    float h = x + radius * cos(CIRCUMFERENCE * theta);
+    float k = y + radius * sin(CIRCUMFERENCE * theta);
+
+    glVertex2d(h, k);
+  }
 
   glEnd();
+}
+
+void Ball::SetColor(float red, float green, float blue) {
+  this->red = red;
+  this->green = green;
+  this->blue = blue;
+}
+
+void Display() {
+  glClear(GL_COLOR_BUFFER_BIT);
+
+  blue_ball.Draw();
+  red_ball.Draw();
+
   glFlush();
 }
 
+void Idle() {
+  usleep(10000);
+
+  blue_ball.x += blue_ball.dx;
+  blue_ball.y += blue_ball.dy;
+
+  red_ball.x += red_ball.dx;
+  red_ball.y += red_ball.dy;
+
+  glutPostRedisplay();
+}
+
 void Setup() {
+  blue_ball.SetColor(0.0f, 0.0f, 1.0f);
+  red_ball.SetColor(1.0f, 0.0f, 0.0f);
+
   glutDisplayFunc(Display);
+  glutIdleFunc(Idle);
+
   DefaultSetup();
 }
 
--- src/homework-one.h
+++ src/homework-one.h
@@ -1,6 +1,34 @@
 #ifndef CS4550_HOMEWORK_ONE_H_
 #define CS4550_HOMEWORK_ONE_H_
 
+#include <math.h>
+
 #include "opengl-window.h"
 
-#endif // CS4550_HOMEWORK_ONE_H_
+#define CIRCUMFERENCE M_PI * 2
+
+class Ball {
+  float red, green, blue;
+public:
+  float x, y;
+  float dx, dy;
+  float radius;
+
+  Ball(float x, float y, float radius=10.0f) {
+    this->x = x;
+    this->y = y;
+    this->radius = radius;
+
+    dx = 1.0f;
+    dy = 1.0f;
+  }
+  ~Ball() {}
+
+  void Draw();
+  void SetColor(float red, float green, float blue);
+};
+
+Ball blue_ball(10.0f, 10.0f);
+Ball red_ball(100.0f, 100.0f);
+
+#endif  // CS4550_HOMEWORK_ONE_H_
